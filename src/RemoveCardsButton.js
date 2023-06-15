b88efa5429e7f6e33441746ac4b54da91c3af015
Anthony Ramos <ramosanthony@gmail.com>
2023-06-15 12:31:02 -0400

    Added useAxios and RemoveCardsButton Components

---
+++ src/RemoveCardsButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function RemoveCardsButton({ removeCards }) {
+  return (
+    <button type="button" onClick={removeCards}>
+      Remove Cards
+    </button>
+  );
+}
